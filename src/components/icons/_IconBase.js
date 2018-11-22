5c4c16a054c4fe7f3afad303319dfbe94a70c605
Jean-Marc Giorgi <jmgiorgi@gmail.com>
2018-11-22 15:53:52 +1100

    lib //1

---
+++ src/components/icons/_IconBase.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default props => {
+    const computedSize = props.size || '1em'
+
+    const styleProp = props.style || {
+        verticalAlign: 'middle'
+    }
+
+    const computedColor = props.color || props.style.color
+    if (computedColor) {
+        styleProp.color = computedColor
+    }
+
+    return (
+        <svg
+            children={props.children}
+            fill='currentColor'
+            preserveAspectRatio='xMidYMid meet'
+            height={props.height || computedSize}
+            width={props.width || computedSize}
+            {...props}
+            style={styleProp}
+        />
+    )
+}
+
